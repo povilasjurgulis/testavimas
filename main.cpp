52899ce05f968221106a7d4c129824191cb6abfb
Povilas Jurgulis <povilas.jurgulis@mif.stud.vu.lt>
2025-06-14 20:47:04 +0300

    Polimorfizmas su virtual

--- main.cpp
+++ main.cpp
@@ -1,42 +1,35 @@
 #include "main.h"
-// Klausimas: Jei kelios bazinės klasės turi funkciją tuo pačiu pavadinimu, 
-// tai kuri iškviečiama išvestinėje klasėje?
-// Ats.: Nei viena! Nebent iš išvestinės klasės specifiškai kvieti bazinių klasių metodus,
-// pvz: Bazinės klasės A ir B (ir jos abi turi Hello() metodą), o C yra jų abiejų išvestinė,
-// tai C.Hello() neveiks, bet C.A::Hello() ir C.B::Hello() veiks. 
-class A{
+// Polimorfizmas leidžia to paties pavadinimo funkcijoms elgtis skirtingai (priklausomai nuo konteksto).
+// Dinaminis polimorfizmas (su virtual) - funkcijos elgesys priklauso nuo to, kokio tipo objektas iš tikrųjų yra, 
+// nors jis naudojamas per bazinės klasės rodyklę ar nuorodą.
+
+class Zmogus{
+    protected:
+    string vardas;
     public:
-    void Hello(){ cout << "Hello is klases A!\n";}
+    Zmogus(string v) : vardas{v}{}
+    virtual void koks_vardas() { cout << vardas << " yra Zmogus\n";}
 };
 
-class B{
+class Studentas : public Zmogus{
     public:
-    void Hello(){ cout << "Hello is klases B!\n";}
+    Studentas(string var) : Zmogus{var}{} 
+    void koks_vardas() { cout << vardas << " yra Studentas\n";}
 };
 
-class C : public A, public B{
-    public:
-    C() = default;
-    // Kadangi klasė C neturi savo Hello() ir klasių A ir B metodas Hello taip pat aprašyti
-    // , tai C.Hello() NEVEIKS!
-};
-
-class D : public C{
-    public:
-    void Hello(){ cout << "Hello is klases D!\n";}
-    D() = default;
-};
 int main() {
-    
-    C objektas;
-    // C.Hello(); NEVEIKIA! Kadangi tokį patį vardą turi ir bazinė klasė A ir B
-    objektas.A::Hello(); // Veikia
-    objektas.B::Hello(); // Irgi veikia
+    Zmogus zm{"Povilas"};
+    Studentas st{"Arnas"};
 
-    D dObj;
-    dObj.Hello(); // Veikia
-    // dObj.C::Hello(); Neveikia!
-    dObj.A::Hello(); // Veikia
-    dObj.B::Hello(); // Veikia
+    zm.koks_vardas(); // Povilas yra Zmogus
+    st.koks_vardas(); // Arnas yra Studentas
+
+    Zmogus& zm1 = st; // Sukuriame nuorodą į Zmogus klasės objektą, bet susiejame su Studentas klasės objektu
+    zm1.koks_vardas(); // Jei nebūtų virtual, tai čia būtų "Arnas yra Zmogus"
+
+    Zmogus* zm2 = &st;
+    zm2->koks_vardas(); // Jei nebūtų virtual, tai čia būtų "Arnas yra Zmogus"
+
+    // Studentas& st1 = zm; Šitaip NEGALIMA, nes Studentas yra išvestinė iš Zmogus
     return 0;
 }
